a03513deb23db6eeff858be89605aba41638fd3a
rook <rooksoto@gmail.com>
2024-02-17 14:54:09 -0500

    Initial Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package com.rooksoto
+
+fun main() {
+    println("Hello, LeetCode!")
+}
---
+++ src/main/kotlin/Problem.kt
@@ -0,0 +1,17 @@
+package com.rooksoto
+
+interface Problem<I, O> {
+
+    val name: String
+
+    val instructions: String
+
+    val testInput: I
+
+    val testSolution: O
+
+    fun solve(input: I): O
+
+    fun isSolved(): Boolean =
+        solve(testInput) == testSolution
+}
